984f9f0aa6aca0f51fe1fb47f289916adb3ac8e2
JulienM <julienm@marmelab.com>
2018-07-09 11:16:40 +0200

    fix lint

--- pages/Grid.js
+++ pages/Grid.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 class Grid extends React.Component {
-  render() {
-    return (
-      <div className="Grid">
-        <div>
-          <h2>This is a grid, trust me</h2>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Grid">
+                <div>
+                    <h2>This is a grid, trust me</h2>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Grid;
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,31 @@
 import React from 'react';
-import Link from 'next/link'
-import styled, { css } from 'react-emotion'
+import Link from 'next/link';
+import styled, { css } from 'react-emotion';
 
 class HomeQuarto extends React.Component {
-  render() {
-    return (
-      <div className={myStyle}>
-        <div>
-          <img src='/static/boardTitle.jpg' alt="logo" />
-          <h2>Welcome to Quarto-isomorphic</h2>
-        </div>
-        <p className="Home-intro">
-          For now, you can't do nothing, but you should watch games soon
-        </p>
-
-        Click{' '}
-    <Link href="/Game">
-      <a>here</a>
-    </Link>{' '}
-    to read more
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={myStyle}>
+                <div>
+                    <img src="/static/boardTitle.jpg" alt="logo" />
+                    <h2>Welcome to Quarto-isomorphic</h2>
+                </div>
+                <p className="Home-intro">
+                    For now, you can’t do nothing, but you should watch games
+                    soon
+                </p>
+                Click{' '}
+                <Link href="/Game">
+                    <a>here</a>
+                </Link>{' '}
+                to read more
+            </div>
+        );
+    }
 }
 
 const myStyle = css`
-  color: darkblue;
-`
+    color: darkblue;
+`;
 
 export default HomeQuarto;
-
-
