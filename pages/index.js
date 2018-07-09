e6bc1a9a26f9f0922f0bfbdebcd9c1d7e95e54d3
JulienM <julienm@marmelab.com>
2018-07-09 09:48:29 +0200

    ignore builded folder

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Link from 'next/link'
-//import '../styles.less';
+import injectSheet from 'react-jss'
 
 class HomeQuarto extends React.Component {
   render() {
     return (
-      <div className="Home">
+      <div className={classes.myButton}>
         <div>
           <img src='/static/boardTitle.jpg' alt="logo" />
           <h2>Welcome to Quarto-isomorphic</h2>
         </div>
         <p className="Home-intro">
-          For now, you can do nothing, but you should watch games soon
+          For now, you can't do nothing, but you should watch games soon
         </p>
 
         Click{' '}
@@ -24,4 +24,26 @@ class HomeQuarto extends React.Component {
   }
 }
 
-export default HomeQuarto;
+//const StyledButton = injectSheet(styles)(Button)
+
+const styles = {
+  myButton: {
+    color: 'green',
+    margin: {
+      // jss-expand gives more readable syntax
+      top: 5, // jss-default-unit makes this 5px
+      right: 0,
+      bottom: 0,
+      left: '1rem'
+    },
+    '& span': {
+      // jss-nested applies this to a child span
+      fontWeight: 'bold' // jss-camel-case turns this into 'font-weight'
+    }
+  },
+  myLabel: {
+    fontStyle: 'italic'
+  }
+}
+
+export default injectSheet(styles)(HomeQuarto);
