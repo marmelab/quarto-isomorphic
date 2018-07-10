b5d554b656e5ceb82f451ed3b65499eb65c1871f
JulienM <julienm@marmelab.com>
2018-07-10 09:19:02 +0200

    Reviews

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,16 @@ import Link from 'next/link';
 import Button from '../src/ui/Button';
 import Container from '../src/ui/Container';
 
-class HomeQuarto extends React.Component {
-    render() {
-        return (
-            <Container>
-                <div>
-                    <img src="/static/boardTitle.jpg" alt="logo" />
-                    <h2>Welcome to Quarto-isomorphic</h2>
-                </div>
-                <Link href="/Game">
-                    <Button>Watch a game</Button>
-                </Link>
-            </Container>
-        );
-    }
-}
+const HomeQuarto = () => (
+    <Container>
+        <div>
+            <img src="/static/boardTitle.jpg" alt="logo" />
+            <h2>Welcome to Quarto-isomorphic</h2>
+        </div>
+        <Link href="/Game">
+            <Button>Watch a game</Button>
+        </Link>
+    </Container>
+);
 
 export default HomeQuarto;
