e1dbd5f95afc1094341a8ba612e831537878af04
JulienM <julienm@marmelab.com>
2018-07-12 15:52:35 +0200

    review on gameservice

--- src/services/gameservice.js
+++ src/services/gameservice.js
@@ -1,7 +1,7 @@
 import fetch from 'isomorphic-unfetch';
 import config from '../../config/config.dist';
 
-const doNothingOnError = error => {
+const logError = error => {
     console.log('Fetch error : '); // eslint-disable-line
     console.log(error); // eslint-disable-line
     return {};
@@ -11,10 +11,5 @@ export const getGame = async idGame => {
     let url = `${config.apiUrl}/${idGame}`;
     return fetch(url)
         .then(res => res.json())
-        .then(res => {
-            return res;
-        })
-        .catch(function(error) {
-            return doNothingOnError(error);
-        });
+        .catch(logError);
 };
